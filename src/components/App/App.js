054c255a95bc4b760de8aaeb788b4bb2da42a5dc
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-17 12:47:00 -0600

    Create error link function in app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,9 +7,18 @@ import {ApolloClient,
         from} from '@apollo/client';
 import {onError} from '@apollo/client/link/error';
 
+
+const errorLink = onError(({graphqlErrors, networkError}) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({message, location, path}) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+}) ;
+
 const link = from([
   errorLink,
-  new HttpLink({uri:"http://localhost:5432/"}) //This will be the link to our backend server when it is up and running! 
+  new HttpLink({uri:"http://localhost:5432/"}) //This will be the link to our backend server when it is up and running! Ask BE team about if we should keep it local or deployed.
 ])
 
 const client = new ApolloClient({
