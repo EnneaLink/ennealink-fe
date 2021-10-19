faa80e0ab128a1380552dd6db8868437a0b6a0ef
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-19 14:23:47 -0600

    Create nav bar skeleton and add to header return

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Header.css';
+import NavBar from '../NavBar/NavBar';
 
 const Header = () => {
 
   return (
     <header className="App-header">
       <h1>EnneaLink</h1>
+      <NavBar />
     </header>
   )
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+
+  return (
+    <ul className="nav-bar">
+      <button>my profile</button>
+      <button>friends</button>
+      <button>log out</button>
+    </ul>
+  )
+}
+
+export default NavBar;
