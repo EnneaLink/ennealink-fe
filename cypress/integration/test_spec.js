0e8ff8c719f1d230dd2542b01e0c5f06bdb4dc58
Hayley Witherell <hayleywitherell@gmail.com>
2021-10-19 14:02:55 -0600

    Add Profile tests

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -7,4 +7,36 @@ describe('Test', () => {
   it('should go to localhost', () => {
     cy.url().should('eq', 'http://localhost:3000/')
   });
+
+  it('should render header', () => {
+    cy.get('h1')
+      .contains('EnneaLink')
+  })
+
+  it('should render username', () => {
+    cy.get('h2')
+      .contains('funbucket')
+  })
+
+  it('should render mbti info', () => {
+    cy.get('h3')
+      .contains('ISFP')
+    .get('h4')
+      .contains('The Adventurer')
+    .get('p')
+      .contains('open minds')      
+    .get('a')
+      .contains('Learn More')    
+  })
+
+  it('should render enneagram info', () => {
+    cy.get('h3')
+      .contains('1')
+    .get('h4')
+      .contains('The Reformer')
+    .get('p')
+      .contains('right and wrong')      
+    .get('a')
+      .contains('Learn More')    
+  })
 })
