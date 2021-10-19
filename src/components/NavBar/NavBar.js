7467d7b376e61558479afa14184867445e1fb3af
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-19 14:51:46 -0600

    Add li tags to nav bar for tabbing accessability

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,9 +5,15 @@ const NavBar = () => {
 
   return (
     <ul className="nav-bar">
-      <button className="nav-btn">my profile</button>
-      <button className="nav-btn">friends</button>
-      <button className="nav-btn">log out</button>
+      <li>
+        <button className="nav-btn">my profile</button>
+      </li>
+      <li>
+        <button className="nav-btn">friends</button>
+      </li>  
+      <li>
+        <button className="nav-btn">log out</button>
+      </li>
     </ul>
   )
 }
