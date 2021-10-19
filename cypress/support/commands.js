de5fa26a7fef8d3c2821d333ed3eaaf607e05b51
Hayley Witherell <hayleywitherell@gmail.com>
2021-10-19 13:57:39 -0600

    Start stub skeleton

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,23 @@
+/////////// HELPER FUNCTIONS /////////// 
+
 Cypress.Commands.add('loadApp', () => {
   cy.visit('http://localhost:3000')
 })
+
+// dynamic stubbing
+
+Cypress.Commands.add('interceptAPI', (fixturePage, url) => {
+  cy.intercept(`${url}`, {
+    fixture: `${fixturePage}_test_data.json`,
+  })
+})
+
+// stubbing endpoints
+
+Cypress.Commands.add('loadProfile', (g) => {
+  cy.loadApp()
+    .interceptAPI('profile', 'http://localhost:4500')
+})
+
+
+
