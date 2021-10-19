a8c45fde3f3382942b6214fa20b8a1e55cd38f3f
Hayley Witherell <hayleywitherell@gmail.com>
2021-10-18 18:21:26 -0600

    Deconstruct

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,34 +2,34 @@ import React from 'react';
 
 const Profile = ({ profileView }) => {
 
+  const {myersBrigg, enneagram, username} = profileView
+
   //We will have a function that dynamically renders a link to show how a user and a friend would interact based off of bothe of their types. This will return a link that we can plug into an <a></a> tag in the return below. (Hayley has good concept for this)
 
-  // if (profileView !== {}) {
-    return (
+  return (
 
-      <article>
-        <h2 className="user-name">{profileView.username}</h2>
-        <div className="type-container">
-          <h3 className="myers-briggs-type">{profileView.myersBrigg.typeOf}</h3>
-          <h4 className="myers-briggs-name">{profileView.myersBrigg.name}</h4>
-          <p>{profileView.myersBrigg.description}</p>
-          <div className="link-container">
-            <a className="myers-briggs-link" src={profileView.myersBrigg.link}>Learn More
-            </a>
-          </div>
+    <article>
+      <h2 className="user-name">{username}</h2>
+      <div className="type-container">
+        <h3 className="myers-briggs-type">{myersBrigg.typeOf}</h3>
+        <h4 className="myers-briggs-name">{myersBrigg.name}</h4>
+        <p>{myersBrigg.description}</p>
+        <div className="link-container">
+          <a className="myers-briggs-link" src={myersBrigg.link}>Learn More
+          </a>
         </div>
-        <div className="type-container">
-          <h3 className="enneagram-type">{profileView.enneagram.number}</h3>
-          <h4 className="myers-briggs-name">{profileView.enneagram.name}</h4>
-          <p>{profileView.enneagram.description}</p>
-          <div className="link-container">
-            <a className="enneagram-link" src={`https://www.enneagraminstitute.com/type-${profileView.enneagram.number}`}>Learn More
-            </a>
-          </div>
+      </div>
+      <div className="type-container">
+        <h3 className="enneagram-type">{enneagram.number}</h3>
+        <h4 className="myers-briggs-name">{enneagram.name}</h4>
+        <p>{enneagram.description}</p>
+        <div className="link-container">
+          <a className="enneagram-link" src={enneagram.link}>Learn More
+          </a>
         </div>
-      </article>
-    )
-  // }
+      </div>
+    </article>
+  )
 }
 
 export default Profile;
