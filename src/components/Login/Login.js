500c20b89f91d5ca307265754dbb8e90c3a3ef04
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-19 18:06:02 -0600

    Add styling to inputs and buttons for login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,9 +20,9 @@ const Login = () => {
     <section className="sign-in">
       <h1>EnneaLink</h1>
         <form className="sign-in-box">
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="enter your username"/>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="enter your password" minlength={8} required/>
-          <button type="submit" className="submit-btn" onClick={handleClick}>sign in</button>
+          <input className="login-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="enter your username"/>
+          <input className="login-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="enter your password" minlength={8} required/>
+          <button className="submit-btn" type="submit" className="submit-btn" onClick={handleClick}>sign in</button>
         </form>
         <button onClick={toggleCreate} className="create-btn">create account</button>
     </section>
