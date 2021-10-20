80691e2ca2c4cb9d0beec9cbca113d6b84731cc0
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-19 18:49:21 -0600

    Add style cohesion to two input forms in ternary statement

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,6 +6,8 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [newUser, setNewUser] = useState(false);
+  const [newPassword, setNewPassword] = useState('');
+  const [passCheck, setPassCheck] = useState('');
 
   const handleClick = (e) => {
     e.preventDefault();
@@ -13,7 +15,20 @@ const Login = () => {
   }
 
   const toggleCreate = () => {
-    setNewUser(true)
+    if (!newUser){
+      setNewUser(true)
+    } else {
+      setNewUser(false)
+    }
+  }
+
+  const createAccount = (e) => {
+    e.preventDefault();
+    if (newPassword === passCheck) {
+      //this can be where we send a POST to the BE to make a new user
+    } else {
+      //return error
+    }
   }
 
   const signIn = (
@@ -29,16 +44,22 @@ const Login = () => {
 
   );
 
-  const createAccount = (
+  const makeAccount = (
     <section className="create-account">
       <h1>EnneaLink</h1>
-
+        <form className="sign-in-box">
+          <input className="login-input" type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="choose a username"/>
+          <input className="login-input" type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="create a password (min 8 characters)" minlength={8} required/>
+          <input className="login-input" type="password" value={passCheck} onChange={(e) => setPassCheck(e.target.value)} placeholder="create a password (min 8 characters)" minlength={8} required/>
+          <button className="submit-btn" type="submit" className="submit-btn" onClick={createAccount}>next</button>
+        </form>
+        <button onClick={toggleCreate} className="create-btn">already a user? sign in</button>
     </section>
   );
 
   return (
     <div>
-      {signIn}
+      { newUser ? makeAccount : signIn}
     </div>
   )
 }
