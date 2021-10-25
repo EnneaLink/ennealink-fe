fd4e237975ff85168822b905c4bcd623d14643b7
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-25 14:21:01 -0600

    Remove console.log from sign in component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -20,7 +20,6 @@ const SignIn = ({toggleCreate, assignUser}) => {
     if (data.loginUser.success){
       assignUser(data.loginUser.id);
       history.push(`/profile/${data.loginUser.id}`); 
-      console.log("data in sign in", data);
     } else {
       setMessage('Please check that your username or password are correct, or click the button below create a new account')
     }
