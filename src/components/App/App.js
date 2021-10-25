8325420c5ecb66888a753fa520fb015c8a60ca27
Mark Cawthray <dr.cawthray@gmail.com>
2021-10-25 11:29:23 -0600

    Pass logout and id to nav bar from all views

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -43,13 +43,13 @@ function App() {
 
       <Route exact path='/profile/:id'
         render={({match}) =>
-          <Profile profileView={ match.params.id } logOut={logOut}/>
+          <Profile profileView={ match.params.id } logOut={logOut} id={id}/>
       }
       />
 
       <Route exact path='/friends'
         render={() =>
-          <UserDisplay friends={data.getUserStats.friends} />
+          <UserDisplay friends={data.getUserStats.friends} logOut={logOut} id={id} />
         }
       />
 
